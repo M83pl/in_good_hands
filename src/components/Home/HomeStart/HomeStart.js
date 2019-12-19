84b4c6865ff332d491c758329dbc3150b62af6ab
Marcin Powązka <mrmarcinpowazka@gmail.com>
2019-12-19 15:52:55 +0100

    HomeStart upgrade
    HomeStart section upgrade.

--- src/components/Home/HomeStart/HomeStart.js
+++ src/components/Home/HomeStart/HomeStart.js
@@ -1,41 +1,76 @@
 import React from "react";
 import { createUseStyles } from "react-jss";
+import { NavLink } from "react-router-dom";
 
 import urlImage from "../../../assets/img/Home-Hero-Image.jpg";
 import urlDecoration from "../../../assets/Decoration.svg";
+import {
+  mainTextColor,
+  threeColumnsBackground
+} from "../../../scss/settings/colors";
 
 const useStyles = createUseStyles({
   start: {
     display: "flex",
+    boxSizing: "border-box",
     flexDirection: "row",
     justifyContent: "flex-end",
+    alignItems: "center",
     width: "100%",
     minHeight: "100vh",
-    margintop: "-5.5rem",
     backgroundSize: "contain",
     backgroundImage: `url(${urlImage})`,
     backgroundPosition: -270,
     backgroundRepeat: "no-repeat"
   },
   start__content: {
+    minHeight: "100vh",
     paddingRight: "7.5%",
-    width: "50%",
+    maxWidth: "50%",
     display: "flex",
     flexDirection: "column",
-    justifyContent: "center"
+    justifyContent: "center",
+    alignItems: "center"
   },
 
-  start__content_main: {
-    right: "15%",
-    textAlign: "center",
-    marginTop: "200px",
-    marginBottom: "200px"
+  start__content_title: {
+    fontSize: "1.75rem",
+    textAlign: "center"
   },
 
   start__content_decoration: {
+    width: "30%",
     backgroundImage: `url(${urlDecoration})`,
-    height: "35px",
-    backgroundRepeat: "no-repeat"
+    height: "3rem",
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+    marginTop: "1.5rem",
+    marginBottom: "4rem"
+  },
+  start__content_buttons: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: "0.2rem"
+  },
+  start__content_buttons_button: {
+    textDecoration: "none",
+    textAlign: "center",
+    fontSize: "1.5rem",
+    width: "10rem",
+    height: "5rem",
+    border: `0.75px solid ${mainTextColor}`,
+    padding: "0.8rem 2rem",
+
+    "&:hover": {
+      color: `${mainTextColor}`,
+      border: `0.75px solid ${threeColumnsBackground}`
+    },
+    "&:visited": {
+      color: `${mainTextColor}`,
+      border: `0.75px solid ${mainTextColor}`
+    }
   }
 });
 
@@ -45,9 +80,26 @@ const HomeStart = props => {
   return (
     <section id="start" className={classes.start}>
       <div className={classes.start__content}>
-        <p>Zacznij pomagać!</p>
-        <p>Oddaj niechciane rzeczy w zaufane ręce</p>
+        <h1 className={classes.start__content_title}>Zacznij pomagać!</h1>
+        <h1 className={classes.start__content_title}>
+          Oddaj niechciane rzeczy w zaufane ręce
+        </h1>
         <div className={classes.start__content_decoration} />
+
+        <div className={classes.start__content_buttons}>
+          <NavLink
+            to="/logowanie"
+            className={classes.start__content_buttons_button}
+          >
+            ODDAJ RZECZY
+          </NavLink>
+          <NavLink
+            to="/logowanie"
+            className={classes.start__content_buttons_button}
+          >
+            ZORGANIZUJ ZBÓRKĘ
+          </NavLink>
+        </div>
       </div>
     </section>
   );
