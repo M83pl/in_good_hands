531b77f8e038086f0d583563c178ee43951bfb93
Marcin Powązka <mrmarcinpowazka@gmail.com>
2020-01-19 20:23:05 +0100

    LoginRegister component update
    commented parts remove

--- src/components/Header/LoginRegister.js
+++ src/components/Header/LoginRegister.js
@@ -7,14 +7,6 @@ import {
 } from "../../scss/settings/colors";
 
 const useStyles = createUseStyles({
-  // loginRegister: {
-  //   display: "flex",
-  //   flexDirection: "row",
-  //   alignContent: "flex-end",
-  //   width: "100%",
-  //   marginBottom: "0.75rem"
-  // },
-
   loginRegister__list: {
     display: "flex",
     flexDirection: "row",
@@ -45,41 +37,6 @@ const useStyles = createUseStyles({
 
 const LoginRegister = props => {
   const classes = useStyles();
-  // const content = props => {
-  //   if (props.user.name !== null) {
-  //     return (
-  //       <>
-  //         <li>{props.user.name}</li>
-  //         <li>
-  //           <Link to="/wylogowano">Wyloguj</Link>
-  //         </li>{" "}
-  //         }
-  //       </>
-  //     );
-  //   } else {
-  //     return (
-  //       <>
-  //         <li>
-  //           <Link to="/logowanie" className={classes.loginRegister__list_item}>
-  //             Zaloguj
-  //           </Link>
-  //         </li>
-  //         <li>
-  //           <Link
-  //             to="/rejestracja"
-  //             className={
-  //               classes.loginRegister__list_item +
-  //               " " +
-  //               classes.loginRegister__list_item_border
-  //             }
-  //           >
-  //             Załóż konto
-  //           </Link>
-  //         </li>
-  //       </>
-  //     );
-  //   }
-  // };
   return (
     <ul className={classes.loginRegister__list}>
       <li className={classes.loginRegister__list_item}>
